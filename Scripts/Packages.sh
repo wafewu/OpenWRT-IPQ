4180e6c462c4edd1485a8dffcdfd3ff8acfedc42
wafewu <148876122+wafewu@users.noreply.github.com>
2025-01-09 10:15:06 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -29,7 +29,6 @@ UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
-UPDATE_PACKAGE "openwrt-passwall-packages" "xiaorouji/openwrt-passwall-packages" "main"
 
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
