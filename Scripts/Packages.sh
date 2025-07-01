87666002f9fb90a2eb58f88ade900f3449515f9f
wafewu <148876122+wafewu@users.noreply.github.com>
2025-07-01 21:12:56 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -68,6 +68,9 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/small-package" "main" "pkg"
 UPDATE_PACKAGE "adguardhome" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-diskman" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "vlmcsd" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-vlmcsd" "kenzok8/small-package" "main" "pkg"
 
 #更新软件包版本
 UPDATE_VERSION() {
@@ -113,96 +116,3 @@ UPDATE_VERSION() {
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
-
-# Git稀疏克隆，只克隆指定目录到指定目录
-git_sparse_clone() {
-    local branch="$1"    # 分支名
-    local repourl="$2"   # 仓库地址
-    local mvpath="$3"    # 转移地址（相对脚本目录）
-    shift 3              # 剩余参数为稀疏检出路径
-
-	local repodir cachepath src_dir foldername del_dirs dir
-	# 克隆指定分支的仓库，使用稀疏检出
-    repodir=$(basename "$repourl" .git)
-	echo -e "\n[info] sparse clone: $repodir"
-
-    # 获取当前操作目录
-    local pwd_path
-	pwd_path="$PWD"
-	echo "[info] pwd_path: $pwd_path"
-    cachepath="$pwd_path/cache_repo/$repodir"
-
-    # 冲突处理：已存在则加时间戳
-    if [[ -d "$cachepath" ]]; then
-        local timestamp
-        timestamp=$(date +%Y%m%d-%H%M%S)
-        cachepath="${cachepath}_${timestamp}"
-    fi
-
-	echo -e "[clone] $repourl => $cachepath (branch: $branch)"
-
-    # 执行稀疏克隆
-	git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl" "$cachepath"
-
-	# 检出指定的文件夹
-	git -C "$cachepath" sparse-checkout set "$@"
-
-    local full_mvpath="$pwd_path/${mvpath%/}"  # 去掉末尾/
-    mkdir -p "$full_mvpath"
-
-	# 循环移动所有需要检出的文件夹
-	for folder in "$@"; do
-		foldername=$(basename "$folder")
-		echo -e "sparse-checkout: $foldername"
-		# 删除 feeds 中存在的同名目录
-		del_dirs=$(find "$pwd_path/feeds/luci" "$pwd_path/feeds/packages" -maxdepth 3 -type d -iname "*$foldername*" 2>/dev/null)
-        if [[ -n "$del_dirs" ]]; then
-            while read -r dir; do
-                rm -rf "$dir"
-                echo "[delete] $dir"
-            done <<< "$del_dirs"
-        else
-            echo "[miss] not found in feeds: $foldername"
-        fi
-
-        # 计算 maxdepth = 传入路径中 '/' 个数 + 1
-        maxdepth_count=$(( $(echo "$folder" | awk -F"/" '{print NF}') ))
-
-		# 查找克隆目录中匹配的文件夹
-		# src_dir=$(find "$cachepath" -maxdepth 3 -type d -iname "*$foldername*" -prune | head -n 1)
-		src_dir=$(find "$cachepath" -maxdepth "$maxdepth_count" -type d -exec bash -c '[[ "$(basename "{}")" == "'"$foldername"'" ]] && echo "{}"' \; | head -n 1)
-        if [[ -n "$src_dir" ]]; then
-            cp -rf "$src_dir" "$full_mvpath/"
-            echo "[copy] $src_dir => $full_mvpath/"
-        else
-            echo "[fail] source not found: $foldername"
-            continue
-        fi
-
-        # 修复 Makefile 引用路径（仅针对 package 目录）
-        if [[ "$mvpath" == "package" ]]; then
-			echo "need update Makefile"
-            find "$full_mvpath/$foldername" -name "Makefile" -exec sed -i \
-                -e 's|include ../../luci.mk|include $(TOPDIR)/feeds/luci/luci.mk|g' \
-                -e 's|include ../../lang/golang/golang-package.mk|include $(TOPDIR)/feeds/packages/lang/golang/golang-package.mk|g' {} +
-        fi
-		
-		echo "[done] sparse update: $foldername success!"
-
-
-		# if [[ $mvpath == "package/" ]]; then
-		# 	find ./package/$foldername/ -name "Makefile" -exec sed -i 's|include ../../luci.mk|include $(TOPDIR)/feeds/luci/luci.mk|g' {} +
-		# 	find ./package/$foldername/ -name "Makefile" -exec sed -i 's|include ../../lang/golang/golang-package.mk|include $(TOPDIR)/feeds/packages/lang/golang/golang-package.mk|g' {} +
-		# fi
-	done
-
-	rm -rf "$pwd_path/cache_repo"
-}
-
-REPO_PATCH="$GITHUB_WORKSPACE/wrt/"
-
-cd $REPO_PATCH
-
-# git_sparse_clone "分支名" "仓库地址" "转移地址(编译根目录下)" "单/多个需要文件夹的目录"
-#git_sparse_clone main https://github.com/VIKINGYFY/packages package luci-app-wolplus luci-app-timewol
-git_sparse_clone main https://github.com/sbwml/openwrt_pkgs package luci-app-diskman luci-app-vlmcsd vlmcsd 
