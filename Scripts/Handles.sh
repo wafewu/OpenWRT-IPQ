e467352fe98ba1cf9f852bb08b456541bcc98966
wafewu <148876122+wafewu@users.noreply.github.com>
2025-07-07 20:58:57 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -125,7 +125,7 @@ else
 fi
 
 #修复DiskMan编译失败
-DM_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-diskman/Makefile")
+DM_FILE="./luci-app-diskman/applications/luci-app-diskman/Makefile"
 if [ -f "$DM_FILE" ]; then
 	echo " "
 
